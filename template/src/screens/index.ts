d65f075c328217887c0b02b37c8b0123cd07d96d
huydqdev <huydq.tech@stepup.edu.vn>
2024-09-18 08:07:20 +0700

    update: new onion gpt screen

--- template/src/screens/index.ts
+++ template/src/screens/index.ts
@@ -1,3 +1,3 @@
 export { default as Startup } from './Startup/Startup';
 export { default as Example } from './Example/Example';
-export { default as Example } from './OnionGPT/OnionGPT';
+export { default as OnionGPT } from './OnionGPT/OnionGPT';
